722f0d7e89774ac73e28b4e527fefb23a7eccf2e
ecotalisman <ecotalisman@gmail.com>
2025-01-24 01:17:27 +0100

    Add solutions for core: level 4 tasks 1, 2

---
+++ src/ua/javarush/python/core/level04/task01/task01.py
@@ -0,0 +1,16 @@
+# Strength in Diversity
+
+# Create 6 variables so that each stores a value of a different type.
+
+### 🇺🇦 Ukrainian version:
+# Сила в різноманітті
+
+# Створи 6 змінних, щоб вони всі зберігали значення різного типу.
+
+# Write your code here
+a = 1
+b = 1.1
+c = "hello"
+d = True
+e = [1, 2, 3]
+f = {1: "value1", 2: "value2", 3: "value3"}
---
+++ src/ua/javarush/python/core/level04/task02/task02.py
@@ -0,0 +1,15 @@
+# Collector
+
+# Create 5 variables of five different collection types (like list, etc.).
+
+### 🇺🇦 Ukrainian version:
+# Колекціонер
+
+# Створи 5 змінних п'яти різних типів колекцій (типу list і т.п.).
+
+# Write your code here
+l = [1, 2, 3]
+t = (1, 2, 3)
+s = {1, 2, 3}
+d = {1: 'a', 2: 'b'}
+f = frozenset([1, 2, 3])

cd739586b16d6099ff68e1483d84c5d1610375b1
ecotalisman <ecotalisman@gmail.com>
2025-02-01 16:37:57 +0100

    Add solutions for core: level 4 tasks 4, 5, 6

---
+++ src/ua/javarush/python/core/level04/task05/task05.py
@@ -0,0 +1,17 @@
+# 8th Grade Math
+
+# Write a program that asks the user for a number and uses the math library to calculate and display the square root and factorial of that number.
+
+### 🇺🇦 Ukrainian version:
+
+# Математика 8 класу
+
+# Напишіть програму, яка запитує у користувача число і використовує бібліотеку math для обчислення та виведення квадратного кореня і факторіалу цього числа.
+
+# Write your code here
+import math
+
+number = input("add number: ")
+
+print(math.sqrt(float(number)))
+print(math.factorial(int(number)))

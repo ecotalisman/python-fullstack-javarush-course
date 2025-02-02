4af5940adc511384541bc764e30af5064d4552e7
ecotalisman <ecotalisman@gmail.com>
2025-02-02 21:02:25 +0100

    Add solutions for core: level 4 tasks 13, 14

---
+++ src/ua/javarush/python/core/level04/task13/task13.py
@@ -0,0 +1,16 @@
+# Random Function
+
+# Write a function generate_random_number() that prints a random number from -200 to 0.
+
+### 🇺🇦 Ukrainian version:
+
+# Випадкова функція
+
+# Напишіть функцію generate_random_number(), яка виводить на екран випадкове число від -200 до 0.
+
+# Write your code here
+
+import random
+def generate_random_number():
+    return print(random.randint(-200, 0))
+generate_random_number()
---
+++ src/ua/javarush/python/core/level04/task14/task14.py
@@ -0,0 +1,16 @@
+# Random Argument
+import random
+
+
+# Write a function print_random(a, b, c) that prints a randomly selected argument from the ones provided.
+
+### 🇺🇦 Ukrainian version:
+
+# Випадковий аргумент
+
+# Напишіть функцію print_random(a,b,c), яка виводить на екран випадково вибраний переданий аргумент.
+
+# Write your code here
+def print_random(a, b, c):
+    return print(random.choice([a, b, c]))
+print_random(1, 2, 3)

d099a9860db50534d2269ced00949cec06e8306e
ecotalisman <>
2025-02-09 20:28:37 +0100

    Add solutions for core: level 5 tasks 13, 14

---
+++ src/ua/javarush/python/core/level05/task13/task13.py
@@ -0,0 +1,14 @@
+# Square Generator
+
+# Write a program that creates a list of squares of numbers from 1 to 10 using List Comprehension.
+# The program should display the generated list.
+
+### 🇺🇦 Ukrainian version:
+
+# Генератор квадратів
+
+# Напишіть програму, яка створює список квадратів чисел від 1 до 10 з використанням List Comprehension.
+# Програма повинна вивести отриманий список.
+
+# Write your code here
+print([x ** 2 for x in range(1, 11)])

5e80d4b1b254966d8ea47b611b92d37a73378458
ecotalisman <>
2025-02-27 00:03:29 +0100

    Add solutions for core: level 6 tasks 3, 4

---
+++ src/ua/javarush/python/core/level06/task03/task03.py
@@ -0,0 +1,15 @@
+# SevenColors
+
+# Write a program that creates a set with 7 elements - color names.
+# The program should display all elements of the created set on the screen.
+
+### 🇺🇦 Ukrainian version:
+
+# СемиКвіт
+
+# Напишіть програму, яка створює множину з 7 елементів - назв кольорів (color).
+# Програма повинна вивести всі елементи створеної множини на екран.
+
+# Write your code here
+s = {"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"}
+print(s)

f5c3276db2202098bb32fd61984e4655c2c1fa6e
ecotalisman <67708040+ecotalisman@users.noreply.github.com>
2025-05-10 00:30:41 +0200

    Add solutions for core: level 6 tasks 15, 16

---
+++ src/ua/javarush/python/core/level06/task15/task15.py
@@ -0,0 +1,20 @@
+# Multilining
+
+# Write a program that creates and prints strings declared in different ways:
+# - A string using single quotes.
+# - A string using double quotes.
+# - A multiline string using triple quotes.
+
+### 🇺🇦 Ukrainian version:
+
+# Багатострочність.
+
+# Напишіть програму, яка створює та виводить на екран рядки, оголошені різними способами:
+# Рядок з використанням одинарних лапок.
+# Рядок з використанням подвійних лапок.
+# Багаторядковий рядок з використанням потрійних лапок.
+
+# Write your code here
+print("Hello")
+print('Hello')
+print("""Hello""")
---
+++ src/ua/javarush/python/core/level06/task16/task16.py
@@ -0,0 +1,17 @@
+# Escaping
+
+# Write a program that creates and prints the following strings:
+# - A string with nested quotes using different types of quotes.
+# - A string with nested quotes using escaping.
+
+### 🇺🇦 Ukrainian version:
+
+# Екранування
+
+# Напишіть програму, яка створює і виводить на екран рядки:
+# Рядок з вкладеними лапками з використанням різних типів лапок.
+# Рядок з вкладеними лапками з використанням екранування.
+
+# Write your code here
+print("Hello 'world!'")
+print("Hello \"world!\"")

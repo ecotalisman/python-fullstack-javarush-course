df2ff3f6d025475c2b58228b29af6a4ee050d4d9
ecotalisman <>
2025-06-07 16:12:30 +0200

    Add solutions for core: level 6 tasks 23 - 24

---
+++ src/ua/javarush/python/core/level06/task24/task24.py
@@ -0,0 +1,14 @@
+# This is Not Funny
+
+# Write a program that prints several emojis (😀, 🤣, 😂) on the screen using their Unicode codes.
+
+### 🇺🇦 Ukrainian version:
+
+# Це не смішно
+
+# Напишіть програму, яка виводить на екран декілька смайлів (😀, 🤣, 😂), використовуючи їх Unicode-коди.
+
+# Write your code here
+print("\U0001F600")  # 😀
+print("\U0001F923")  # 🤣
+print("\U0001F602")  # 😂

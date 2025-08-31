fd34dad48405e774d17cea8e544ba21f7934a4ad
ecotalisman <>
2025-08-31 23:00:05 +0200

    Add solutions for core: level 10 tasks 1 - 2

---
+++ src/ua/javarush/python/core/level10/task01/task01.py
@@ -0,0 +1,12 @@
+# Division by Zero
+
+# Write a program that raises a ZeroDivisionError exception.
+
+### 🇺🇦 Ukrainian version:
+
+# Ділення на нуль.
+
+# Напиши програму, яка створює виняток ZeroDivisionError
+
+# Write your code here
+print(1/0)

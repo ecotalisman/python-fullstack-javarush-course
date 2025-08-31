2cd964f766259641bcfd790a71b1abfed45429b2
ecotalisman <67708040+ecotalisman@users.noreply.github.com>
2025-08-31 23:15:25 +0200

    Add solutions for core: level 10 tasks 3 - 4

---
+++ src/ua/javarush/python/core/level10/task03/task03.py
@@ -0,0 +1,12 @@
+# Accessing a Non-Existent Variable
+
+# Write a program that results in a NameError exception.
+
+### 🇺🇦 Ukrainian version:
+
+# Звернення до неіснуючої змінної.
+
+# Напиши програму, виконання якої призводить до виключення NameError.
+
+# Write your code here
+print(A())
---
+++ src/ua/javarush/python/core/level10/task04/task04.py
@@ -0,0 +1,13 @@
+# Syntax Error
+
+# Write a program that raises a SyntaxError exception.
+
+### 🇺🇦 Ukrainian version:
+
+# Синтаксична помилка.
+
+# Напиши програму, яка створює виключення SyntaxError
+
+# Write your code here
+# exec("if True print('oops')")
+exec("if True print('oops')")

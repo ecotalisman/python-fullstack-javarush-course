ce1fa80e94c603faa416bc86977c6d521a21b798
ecotalisman <67708040+ecotalisman@users.noreply.github.com>
2025-08-31 23:00:05 +0200

    Add solutions for core: level 10 tasks 1 - 2

---
+++ src/ua/javarush/python/core/level10/task02/task02.py
@@ -0,0 +1,12 @@
+# Type Error
+
+# Write a program that raises a TypeError exception.
+
+### 🇺🇦 Ukrainian version:
+
+# Помилка роботи з типом.
+
+# Напиши програму, яка створює виключення TypeError
+
+# Write your code here
+print(1 + "A")

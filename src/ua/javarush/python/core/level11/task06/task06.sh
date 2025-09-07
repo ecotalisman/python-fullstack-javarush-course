4e67f554e8655e55b705a074751c1086ef1f2782
ecotalisman <>
2025-09-07 18:59:34 +0200

    Add solutions for core: level 11 tasks 5 - 6

---
+++ src/ua/javarush/python/core/level11/task06/task06.sh
@@ -0,0 +1,36 @@
+# Creating and Managing Environments with conda
+
+# Use the conda package manager to create and manage isolated environments.
+# Perform the following steps:
+# - Create a new environment named myenv.
+# - Activate the myenv environment.
+# - Install the numpy package in the myenv environment.
+# - Display the list of all installed packages in the myenv environment.
+# - Deactivate the myenv environment.
+
+### 🇺🇦 Ukrainian version:
+
+# Створення та керування середовищами за допомогою conda
+
+# Використайте пакетний менеджер conda для створення та керування ізольованими середовищами.
+# Виконайте наступні кроки:
+# Створіть нове середовище під назвою myenv.
+# Активуйте середовище myenv.
+# Встановіть пакет numpy у середовище myenv.
+# Відобразіть список усіх встановлених пакетів у середовищі myenv.
+# Деактивуйте середовище myenv.
+
+# Create a new environment
+# conda create --name myenv
+
+# Activate the environment
+# conda activate myenv
+
+# Install numpy in the environment
+# conda install numpy
+
+# Display the list of installed packages in the environment
+# conda list
+
+# Deactivate the environment
+# conda deactivate

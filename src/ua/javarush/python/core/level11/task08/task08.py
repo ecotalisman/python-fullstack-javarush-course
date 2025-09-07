47778a83018439a82997d7594f72c25cb5006c9f
ecotalisman <67708040+ecotalisman@users.noreply.github.com>
2025-09-07 20:23:04 +0200

    Add solutions for core: level 11 tasks 7 - 8

---
+++ src/ua/javarush/python/core/level11/task08/task08.py
@@ -0,0 +1,25 @@
+# Using the cowsay Package
+
+# Install the cowsay package and use it to display a message.
+# Perform the following steps:
+# - Install the cowsay package using pip.
+# - Use the cowsay package to display the message "Треба було вчити Python...".
+# - Uninstall the cowsay package using pip.
+
+### 🇺🇦 Ukrainian version:
+
+# Використання пакета cowsay
+
+# Встановіть пакет cowsay та використовуйте його для відображення повідомлення.
+# Виконайте наступні кроки:
+# Встановіть пакет cowsay за допомогою pip.
+# Використовуйте пакет cowsay для відображення повідомлення "Треба було вчити Python...".
+# Видаліть пакет cowsay за допомогою pip.
+
+# Install cowsay
+# python -m pip install cowsay
+
+# Uninstall cowsay
+# python -m pip uninstall cowsay -y
+import cowsay
+cowsay.cow("Треба було вчити Python...")

1b85c5c23e10473dbd3a89e38b4a7931991efae9
ecotalisman <67708040+ecotalisman@users.noreply.github.com>
2025-09-07 18:23:26 +0200

    Add solutions for core: level 11 tasks 3 - 4

---
+++ src/ua/javarush/python/core/level11/task04/task04.py
@@ -0,0 +1,16 @@
+# Exploring a Module
+
+# Create a module mymodule.py with a function and a variable.
+# Use the dir() function to get a list of all available attributes and methods in this module.
+
+### 🇺🇦 Ukrainian version:
+
+# Дослідження модуля
+
+# Створіть модуль mymodule.py з функцією та змінною.
+# Використайте функцію dir(), щоб отримати список усіх доступних атрибутів і методів у цьому модулі.
+
+# Write your code here
+import mymodule
+
+print(dir(mymodule))

4e54029a50d847fd56568dfd32d6a476529081b5
ecotalisman <>
2025-09-09 00:41:56 +0200

    Add solutions for core: level 11 tasks 15 - 16

---
+++ src/ua/javarush/python/core/level11/task15/task15.py
@@ -0,0 +1,19 @@
+# Fixing Global Variables
+
+# Fix the function code:
+
+### 🇺🇦 Ukrainian version:
+
+# Виправляємо глобальні змінні.
+
+# Виправте код функції:
+
+# Write your code here
+x = 10
+
+def foo_correct():
+    global x
+    x += 1
+    print(x)
+
+foo_correct()  # output: 11

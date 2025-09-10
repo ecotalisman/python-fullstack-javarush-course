0b191de5f80de6f2545faf81de068779e79b1786
ecotalisman <>
2025-09-11 00:35:54 +0200

    Add solutions for core: level 12 tasks 5 - 6

---
+++ src/ua/javarush/python/core/level12/task05/task05.py
@@ -0,0 +1,15 @@
+# Creating a File
+
+# Write a program that creates a new file example.txt
+# and writes the string "This is a new file." into it.
+
+### 🇺🇦 Ukrainian version:
+
+# Створення файлу
+
+# Напишіть програму, яка створює новий файл example.txt і записує в нього рядок "This is a new file."
+
+# Write your code here
+file = open('example.txt', 'w')
+file.write("This is a new file.")
+file.close()

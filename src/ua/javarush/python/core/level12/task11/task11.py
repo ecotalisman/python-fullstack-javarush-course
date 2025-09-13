22eeb04c34fe806d87d1749cfc117b265de3bfd2
ecotalisman <>
2025-09-13 14:37:16 +0200

    Add solutions for core: level 12 tasks 11 - 12

---
+++ src/ua/javarush/python/core/level12/task11/task11.py
@@ -0,0 +1,14 @@
+# File Copying
+
+# Write a program that copies the file source.txt to destination.txt.
+
+### 🇺🇦 Ukrainian version:
+
+# Копіювання файлу
+
+# Напишіть програму, яка копіює файл source.txt у файл destination.txt
+
+# Write your code here
+import shutil
+
+shutil.copy('source.txt', 'destination.txt')

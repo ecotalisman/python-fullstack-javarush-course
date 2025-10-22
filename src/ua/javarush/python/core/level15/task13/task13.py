e68d3006305738ef873e24885703af2671f4ab54
ecotalisman <>
2025-10-22 19:11:55 +0200

    Add solutions for core: level 15 tasks 13 - 14

---
+++ src/ua/javarush/python/core/level15/task13/task13.py
@@ -0,0 +1,24 @@
+# My Favorite Dynamic Array
+
+# Write a program that creates a dynamic array (list)
+# and demonstrates its core operations: append, remove, index access, and element update.
+# You may use the built-in list class.
+
+### 🇺🇦 Ukrainian version:
+
+# Мій улюблений список
+
+# Напишіть програму, яка створює динамічний масив (список)
+# і демонструє його основні операції: додавання, видалення, доступ за індексом і зміна елемента.
+# Клас list використовувати можна.
+
+# Write your code here
+arr = []
+arr.append(1)
+arr.append(2)
+arr.append(3)
+arr.append(4)
+arr[3] = 6
+arr.pop(2)
+print(arr[0])
+print(arr)

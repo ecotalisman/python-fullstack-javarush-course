1fb717ae20a0fc3c858fe82043743cf2c405ebb1
ecotalisman <67708040+ecotalisman@users.noreply.github.com>
2026-01-28 22:51:19 +0100

    chore(module-2-fullstack): add level17 tasks 13-14 solutions

---
+++ src/ua/javarush/web/core/level17/task13/solution.js
@@ -0,0 +1,51 @@
+/*
+Array update and push
+
+Create an array of fruit names: ["Apple", "Banana", "Cherry"].
+Change the second element of the array to "Blueberry".
+Print the modified array to the console.
+Then add another fruit "Mango" to the end of the array and print the new array length.
+
+Requirements:
+• The program must create an array of fruit names: ["Apple", "Banana", "Cherry"].
+• The program must change the second element of the array to "Blueberry".
+• The program must print the modified array to the console.
+• The program must add the fruit "Mango" to the end of the array.
+• The program must print the new length of the array to the console.
+
+🇺🇦 Ukrainian version:
+
+Створіть масив з іменами фруктів: ["Apple", "Banana", "Cherry"].
+Змініть другий елемент масиву на "Blueberry".
+Виведіть змінений масив у консоль.
+Потім додайте ще один фрукт "Mango" в кінець масиву і виведіть нову довжину масиву.
+
+Вимоги:
+• Програма повинна створити масив з іменами фруктів: ["Apple", "Banana", "Cherry"].
+• Програма повинна змінити другий елемент масиву на "Blueberry".
+• Програма повинна вивести змінений масив у консоль.
+• Програма повинна додати фрукт "Mango" в кінець масиву.
+• Програма повинна вивести нову довжину масиву в консоль.
+
+Write your code here
+*/
+
+// Declare the array
+//TODO:
+let arr = ["Apple", "Banana", "Cherry"];
+
+// Change the second element of the array
+//TODO:
+arr[1] = "Blueberry";
+
+// Print the modified array
+//TODO:
+console.log(arr);
+
+// Add "Mango" to the end of the array
+//TODO:
+arr.push("Mango");
+
+// Print the new length of the array
+//TODO:
+console.log(arr.length);
---
+++ src/ua/javarush/web/core/level17/task14/solution.js
@@ -0,0 +1,49 @@
+/*
+Build an array using unshift() and push()
+
+Create an empty array. First add "start1" and "start2" to the beginning of the array
+using unshift(). Then add "end1" and "end2" to the end of the array using push().
+Print the final array and its length to the console.
+
+Requirements:
+• The program must create an empty array.
+• The program must add "start1" and "start2" to the beginning of the array using unshift().
+• The program must add "end1" and "end2" to the end of the array using push().
+• The program must print the final array to the console.
+• The program must print the length of the array to the console.
+
+🇺🇦 Ukrainian version:
+
+Створіть порожній масив. Спочатку додайте на початок масиву елементи "start1" і "start2"
+за допомогою методу unshift. Потім додайте в кінець масиву елементи "end1" і "end2"
+за допомогою методу push. Виведіть кінцевий масив і його довжину в консоль.
+
+Вимоги:
+• Програма повинна створити порожній масив.
+• Програма повинна додати елементи "start1" і "start2" на початок масиву з використанням методу unshift.
+• Програма повинна додати елементи "end1" і "end2" в кінець масиву з використанням методу push.
+• Програма повинна вивести в консоль кінцевий масив.
+• Програма повинна вивести в консоль довжину масиву.
+
+Write your code here
+*/
+
+// Create an empty array
+//TODO:
+let arr = [];
+
+// Add elements to the beginning of the array
+//TODO:
+arr.unshift("start1", "start2");
+
+// Add elements to the end of the array
+//TODO:
+arr.push("end1", "end2");
+
+// Print the final array
+//TODO:
+console.log(arr);
+
+// Print the array length
+//TODO:
+console.log(arr.length);

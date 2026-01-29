a462b49e5b646524b3fafd94cbf9d9acdbb9aef3
ecotalisman <67708040+ecotalisman@users.noreply.github.com>
2026-01-29 16:20:09 +0100

    chore(module-2-fullstack): add level18 tasks 1-2 solutions

---
+++ src/ua/javarush/web/core/level18/task01/solution.js
@@ -0,0 +1,32 @@
+/*
+Convert "150" to an integer with parseInt() and print the type
+
+You have a variable str that contains the string "150".
+Convert this string to an integer using parseInt() and assign the result to a new variable num.
+Print the type of num to the console.
+
+Requirements:
+• The program must include a variable str that contains the string "150".
+• The program must use parseInt() to convert the string "150" to an integer.
+• The program must assign the conversion result to the variable num.
+• The program must print the type of num using the typeof operator.
+
+🇺🇦 Ukrainian version:
+
+У вас є змінна str, яка містить рядок "150". Перетворіть цей рядок на ціле число,
+використовуючи метод parseInt(), і присвойте результат новій змінній num.
+Виведіть тип змінної num в консоль.
+
+Вимоги:
+• Програма повинна включати змінну str, яка містить рядок "150".
+• Програма повинна використовувати метод parseInt() для перетворення рядка "150" на ціле число.
+• Програма повинна присвоїти результат перетворення змінній num.
+• Програма повинна вивести в консоль тип змінної num з використанням оператора typeof.
+
+Write your code here
+*/
+
+let str = "150";
+//TODO:
+let num = parseInt(str)
+console.log(typeof num)
---
+++ src/ua/javarush/web/core/level18/task02/solution.js
@@ -0,0 +1,46 @@
+/*
+Add a number and a string, then print the value and type
+
+Create a variable num containing the number 200 and a variable str containing the string "300".
+Add these variables and assign the result to result. Print the value and the type of result to the console.
+
+Requirements:
+• The program must include a variable num containing the number 200.
+• The program must include a variable str containing the string "300".
+• The program must add num and str and assign the result to result.
+• The program must print the value of result to the console.
+• The program must print the type of result to the console.
+
+🇺🇦 Ukrainian version:
+
+Створіть змінну num, що містить число 200, і змінну str, що містить рядок "300".
+Виконайте додавання цих змінних і присвойте результат змінній result.
+Виведіть значення та тип змінної result в консоль.
+
+Вимоги:
+• Програма повинна включати змінну num, що містить число 200.
+• Програма повинна включати змінну str, що містить рядок "300".
+• Програма повинна виконати додавання змінних num і str та присвоїти результат змінній result.
+• Програма повинна вивести в консоль значення змінної result.
+• Програма повинна вивести в консоль тип змінної result.
+
+Write your code here
+*/
+
+// Declare the variable num containing the number 200
+let num = 200;
+
+// Declare the variable str containing the string "300"
+let str = "300";
+
+// Add num and str, assign the result to result
+//TODO:
+let result = num + str
+
+// Print the value of result to the console
+//TODO:
+console.log(result)
+
+// Print the type of result to the console
+//TODO:
+console.log(typeof result)

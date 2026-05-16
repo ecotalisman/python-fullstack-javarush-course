c24db41ad2778f0365e95ec95b742217f73b715e
ecotalisman <67708040+ecotalisman@users.noreply.github.com>
2026-05-16 18:28:23 +0200

    chore(module-2-fullstack): add solutions for level 8 tasks 1, 3

---
+++ src/ua/javarush/python/fullstack/level08/task03/solution.sh
@@ -0,0 +1,24 @@
+# Running a React Application in Docker
+# Use the npx create-react-app command to generate a React project.
+# Create a Dockerfile for the container with the React application.
+# Then configure it to run inside Docker.
+#
+# Requirements:
+# • The npx create-react-app command must be used to generate the React project.
+# • A Dockerfile must be created for the container that will contain the React application.
+# • The React application must be configured to run inside a Docker container.
+#
+# 🇺🇦 Ukrainian version:
+#
+# Запуск React застосунку в Docker
+# Використайте команду npx create-react-app для генерації React-проєкту.
+# Створіть Dockerfile для контейнера з React-застосунком.
+# Потім налаштуйте його для запуску всередині Docker.
+#
+# Вимоги:
+# • Команда npx create-react-app має бути використана для генерації React-проєкту.
+# • Dockerfile має бути створений для контейнера, що міститиме React-застосунок.
+# • React-застосунок має бути сконфігурований для запуску всередині контейнера Docker.
+
+# Creating a React application:
+npx create-react-app my-react-app
